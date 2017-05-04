7e6a5aaf1c2ddde996cc576526f1adf078624bb2
Kevin Moposita <kmoposita@aclanyc.com>
2017-05-04 02:18:49 +0000

    Add Randomness

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
 
+var teacher = ['Aaron', 'Zack', 'Justin'];
+
+$("#teacherButton").click(function() {
+    var random = Math.floor(Math.random() * teacher.length);
+    $("#teacherDisplay").append(teacher[random]);
+});
 
 
+var students = ['Jennifer', 'Darleny', 'Wayne', 'Zenebou', 'Bradley'];
+
+$("#studentButton").click(function () {
+    var random = Math.floor(Math.random() * students.length);
+    $("#studentDisplay").append(students[random]);
+});
 });
